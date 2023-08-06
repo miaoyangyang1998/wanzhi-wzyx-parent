6ccf71e75c8100f1b76631e796e2ef63a85938f9
miaoyangyang <miaoyangyang1998@foxmail.com>
2023-08-06 23:58:42 +0800

    首次提交，创建仓库

---
+++ common/service-util/src/main/java/org/wanzhi/Main.java
@@ -0,0 +1,11 @@
+package org.wanzhi;
+
+/**
+ * @author :Yangyang Miao
+ * @date :2023-08-06 23:41:00
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
