cb998cba9650c7dae20cdbba8b637fe598e3abaa
miaoyangyang <miaoyangyang1998@foxmail.com>
2023-08-09 10:33:44 +0800

    [add]添加实体类

---
+++ model/src/main/java/com/wanzhi/wzyx/vo/order/OrderUserQueryVo.java
@@ -0,0 +1,16 @@
+package com.wanzhi.wzyx.vo.order;
+
+import com.wanzhi.wzyx.enums.OrderStatus;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+@Data
+public class OrderUserQueryVo {
+
+	private Long userId;
+
+	@ApiModelProperty(value = "订单状态")
+	private OrderStatus orderStatus;
+
+}
+
